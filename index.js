bbbb70bb1f6193e1dcd5d2231e1581d7d2fb9753
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-07-13 11:19:04 +0300

    commits

--- index.js
+++ index.js
@@ -1,84 +1,55 @@
 const express = require('express');
-const dns = require('dns');
-const { MongoClient } = require('mongodb');
-
+const cors = require('cors');
 const app = express();
+
+// Basic Configuration
 const port = process.env.PORT || 3000;
 
-const client = new MongoClient(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+const urls = [];
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.use(express.json());
+// Your API endpoint to shorten a URL
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
 
-// Connect to the MongoDB database
-client.connect(err => {
-  if (err) {
-    console.error('Failed to connect to the database');
-    process.exit(1);
+  // Check if the URL is valid (you can use a library like 'valid-url' for more robust validation)
+  if (!url || !url.startsWith('http')) {
+    res.json({ error: "Invalid URL" });
+    return;
   }
-  
-  console.log('Connected to the database');
-  
-  const db = client.db('urlshortener');
-  const urls = db.collection('urls');
-  
-  // Route to handle URL shortening
-  app.post('/api/shorturl', async (req, res) => {
-    const { url } = req.body;
-    
-    // Validate the URL format
-    const urlPattern = /^https?:\/\/(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+(\/\S*)?$/;
-    if (!urlPattern.test(url)) {
-      return res.json({ error: 'invalid url' });
-    }
-    
-    try {
-      // Check if the URL already exists in the database
-      const existingUrl = await urls.findOne({ original_url: url });
-      if (existingUrl) {
-        return res.json({
-          original_url: existingUrl.original_url,
-          short_url: existingUrl.short_url
-        });
-      }
-      
-      // Insert the new URL into the database
-      const urlCount = await urls.countDocuments();
-      const newUrl = { original_url: url, short_url: urlCount + 1 };
-      await urls.insertOne(newUrl);
-      
-      res.json({
-        original_url: newUrl.original_url,
-        short_url: newUrl.short_url
-      });
-    } catch (error) {
-      console.error('Error while shortening the URL:', error);
-      res.status(500).json({ error: 'internal server error' });
-    }
-  });
-  
-  // Route to handle URL redirection
-  app.get('/api/shorturl/:short_url', async (req, res) => {
-    const { short_url } = req.params;
-    
-    try {
-      const urlDoc = await urls.findOne({ short_url: parseInt(short_url) });
-      
-      if (!urlDoc) {
-        return res.json({ error: 'url not found' });
-      }
-      
-      res.redirect(urlDoc.original_url);
-    } catch (error) {
-      console.error('Error while redirecting to the original URL:', error);
-      res.status(500).json({ error: 'internal server error' });
-    }
-  });
-  
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
+
+  // Generate a random short URL
+  const shortUrl = Math.random().toString(36).substring(7);
+
+  // Store the original and short URL in the array
+  urls.push({ original_url: url, short_url: shortUrl });
+
+  res.json({ original_url: url, short_url: shortUrl });
+});
+
+// Redirect to the original URL when the short URL is accessed
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  const shortUrl = req.params.shortUrl;
+
+  // Find the corresponding URL in the array
+  const urlDoc = urls.find(url => url.short_url === shortUrl);
+
+  if (!urlDoc) {
+    res.json({ error: "Invalid short URL" });
+    return;
+  }
+
+  res.redirect(urlDoc.original_url);
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
